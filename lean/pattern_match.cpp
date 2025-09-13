4a71aedf1ae9434ab16f0741436384091a102b51
aaravm <aarav.mehta.mat22@itbhu.ac.in>
2025-09-13 16:16:13 +0530

    update cmake files

--- lean/pattern_match.cpp
+++ lean/pattern_match.cpp
@@ -5,12 +5,25 @@
 namespace lean {
 
 std::string analyzeAliveFunctions(const IR::Function *fn1, const IR::Function *fn2) {
-  std::stringstream ss;
+  std::stringstream ss, fn1_ss, fn2_ss;
+  // return fn1_ss;
+  // Get full function representation
+  fn1->print(fn1_ss);
+  fn2->print(fn2_ss);
+ 
   ss << "Analysis of functions:\n";
   ss << "Source function name: " << fn1->getName() << "\n";
+  ss << "Source function contents:\n" << fn1_ss.str() << "\n";
   ss << "Target function name: " << fn2->getName() << "\n";
-  // Add any other analysis you want here
+  ss << "Target function contents:\n" << fn2_ss.str() << "\n";
+  
   return ss.str();
 }
+
+// fn pattern_match(){
+//     if(type==4){
+//         my_project::exec_command1();
+//     }
+// }
 
 } // namespace lean
